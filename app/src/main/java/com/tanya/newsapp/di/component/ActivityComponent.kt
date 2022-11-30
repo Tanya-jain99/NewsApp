42595b34c9b2c48cbd5dddd7143704bac3a79e47
Tanya jain <tjain@lolofit.com>
2022-11-30 17:04:41 +0530

    fix: replaced activity instance

--- app/src/main/java/com/tanya/newsapp/di/component/ActivityComponent.kt
+++ app/src/main/java/com/tanya/newsapp/di/component/ActivityComponent.kt
@@ -2,13 +2,13 @@ package com.tanya.newsapp.di.component
 
 import com.tanya.newsapp.di.ActivityScope
 import com.tanya.newsapp.di.module.ActivityModule
-import com.tanya.newsapp.ui.topheadline.TopHeadlineActivity
+import com.tanya.newsapp.ui.base.MainActivity
 import dagger.Component
 
 @ActivityScope
 @Component(dependencies = [ApplicationComponent::class], modules = [ActivityModule::class])
 interface ActivityComponent {
 
-    fun inject(activity: TopHeadlineActivity)
+    fun inject(activity: MainActivity)
 
 }
