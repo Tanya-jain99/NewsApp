1a91f04984ff02758544d1f6a839b6f74f5d9b4c
Tanya jain <tjain@lolofit.com>
2022-11-30 17:06:04 +0530

    fix: removed unused imports

--- app/src/main/java/com/tanya/newsapp/di/module/ActivityModule.kt
+++ app/src/main/java/com/tanya/newsapp/di/module/ActivityModule.kt
@@ -2,12 +2,7 @@ package com.tanya.newsapp.di.module
 
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.tanya.newsapp.data.repository.TopHeadlineRepository
 import com.tanya.newsapp.di.ActivityContext
-import com.tanya.newsapp.ui.base.ViewModelProviderFactory
-import com.tanya.newsapp.ui.topheadline.TopHeadlineAdapter
-import com.tanya.newsapp.ui.topheadline.TopHeadlineViewModel
 import dagger.Module
 import dagger.Provides
 
