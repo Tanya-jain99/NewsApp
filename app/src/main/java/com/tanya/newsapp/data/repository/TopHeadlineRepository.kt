fa15fc4e7f87c5418ad1df766048d4f04db25e3f
Tanya jain <tjain@lolofit.com>
2022-11-30 17:03:41 +0530

    feat: added data for categories

--- app/src/main/java/com/tanya/newsapp/data/repository/TopHeadlineRepository.kt
+++ app/src/main/java/com/tanya/newsapp/data/repository/TopHeadlineRepository.kt
@@ -2,6 +2,10 @@ package com.tanya.newsapp.data.repository
 
 import com.tanya.newsapp.data.api.NetworkService
 import com.tanya.newsapp.data.model.Article
+import com.tanya.newsapp.data.model.Language
+import com.tanya.newsapp.data.model.Country
+import com.tanya.newsapp.data.model.Source
+import com.tanya.newsapp.utils.DataUtils
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
@@ -11,12 +15,31 @@ import javax.inject.Singleton
 @Singleton
 class TopHeadlineRepository @Inject constructor(private val networkService: NetworkService) {
 
-    fun getTopHeadlines(country: String): Flow<List<Article>> {
+    fun getTopHeadlines(params: Map<String?,String?>): Flow<List<Article>> {
         return flow {
-            emit(networkService.getTopHeadlines(country))
+            emit(networkService.getTopHeadlines(params))
         }.map {
             it.articles
         }
     }
 
+    fun getCountries(): Flow<List<Country>> {
+        return flow {
+            emit(DataUtils.getCountries())
+        }
+    }
+
+    fun getLanguages(): Flow<List<Language>> {
+        return flow {
+            emit(DataUtils.getLanguages())
+        }
+    }
+
+    fun getNewsSources() : Flow<List<Source>>{
+        return flow {
+            emit(networkService.getNewsSources())
+        }.map{
+            it.sources
+        }
+    }
 }
