56486df14a77baf1c6f2949a9e631a186d417e0e
Tanya jain <tjain@lolofit.com>
2022-11-30 17:01:12 +0530

    feat: added new sources endpoint

--- app/src/main/java/com/tanya/newsapp/data/api/NetworkService.kt
+++ app/src/main/java/com/tanya/newsapp/data/api/NetworkService.kt
@@ -1,10 +1,11 @@
 package com.tanya.newsapp.data.api
 
 import com.tanya.newsapp.data.api.Networking.API_KEY
+import com.tanya.newsapp.data.model.SourceResponse
 import com.tanya.newsapp.data.model.TopHeadlinesResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
-import retrofit2.http.Query
+import retrofit2.http.QueryMap
 import javax.inject.Singleton
 
 @Singleton
@@ -12,6 +13,11 @@ interface NetworkService {
 
     @Headers("X-Api-Key: $API_KEY")
     @GET("top-headlines")
-    suspend fun getTopHeadlines(@Query("country") country: String): TopHeadlinesResponse
+    suspend fun getTopHeadlines(@QueryMap options : Map<String?, String?>): TopHeadlinesResponse
+
+    @Headers("X-Api-Key: $API_KEY")
+    @GET("top-headlines/sources")
+    suspend fun getNewsSources():SourceResponse
+
 
 }
