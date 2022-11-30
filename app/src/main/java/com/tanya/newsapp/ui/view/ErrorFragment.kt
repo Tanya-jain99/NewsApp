9d6fc5ee52760f127decf44b2af9c24fd44f49cf
Tanya jain <tjain@lolofit.com>
2022-11-30 16:23:48 +0530

    added error sceanrio

---
+++ app/src/main/java/com/tanya/newsapp/ui/view/ErrorFragment.kt
@@ -0,0 +1,40 @@
+package com.tanya.newsapp.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tanya.newsapp.R
+import com.tanya.newsapp.databinding.FragmentErrorBinding
+import com.tanya.newsapp.ui.base.MainActivity
+
+
+class ErrorFragment : Fragment() {
+
+    private lateinit var binding: FragmentErrorBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentErrorBinding.inflate(layoutInflater)
+        binding.tryAgain.setOnClickListener {
+            val activity = activity as MainActivity
+            activity.onBackPressed()
+        }
+        return binding.root
+    }
+
+    companion object {
+        const val TAG = "ErrorFragment"
+
+        fun newInstance() : ErrorFragment{
+            val args = Bundle()
+            val fragment = ErrorFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
