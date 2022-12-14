d992344e8788f8d120acffb646606667dad1383e
Tanya jain <tjain@lolofit.com>
2022-12-14 17:54:30 +0530

    feat: added search endpoint

--- app/src/main/java/com/tanya/newsapp/data/api/NetworkService.kt
+++ app/src/main/java/com/tanya/newsapp/data/api/NetworkService.kt
@@ -1,23 +1,22 @@
 package com.tanya.newsapp.data.api
 
-import com.tanya.newsapp.data.api.Networking.API_KEY
 import com.tanya.newsapp.data.model.SourceResponse
 import com.tanya.newsapp.data.model.TopHeadlinesResponse
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 import javax.inject.Singleton
 
 @Singleton
 interface NetworkService {
 
-    @Headers("X-Api-Key: $API_KEY")
     @GET("top-headlines")
     suspend fun getTopHeadlines(@QueryMap options : Map<String?, String?>): TopHeadlinesResponse
 
-    @Headers("X-Api-Key: $API_KEY")
     @GET("top-headlines/sources")
     suspend fun getNewsSources():SourceResponse
 
+    @GET("everything")
+    suspend fun search(@Query("q") searchKey : String) : TopHeadlinesResponse
 
 }
