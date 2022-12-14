aff3e2b9441d67e778fa9ebcc29bd344e35a1dd0
Tanya jain <tjain@lolofit.com>
2022-12-14 17:49:21 +0530

    refactor: Network service provided via DI

--- app/src/main/java/com/tanya/newsapp/di/module/ApplicationModule.kt
+++ app/src/main/java/com/tanya/newsapp/di/module/ApplicationModule.kt
@@ -1,12 +1,19 @@
 package com.tanya.newsapp.di.module
 
 import android.content.Context
+import androidx.room.Room
 import com.tanya.newsapp.NewsApplication
+import com.tanya.newsapp.data.api.KeyInterceptor
 import com.tanya.newsapp.data.api.NetworkService
-import com.tanya.newsapp.data.api.Networking
+import com.tanya.newsapp.data.repository.AppDatabase
+import com.tanya.newsapp.data.repository.ArticleDao
 import com.tanya.newsapp.di.ApplicationContext
+import com.tanya.newsapp.di.BaseUrl
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -20,6 +27,43 @@ class ApplicationModule(private val application: NewsApplication) {
 
     @Provides
     @Singleton
-    fun provideNetworkService(): NetworkService = Networking.create()
+    fun providesAppDatabase() : AppDatabase = Room.databaseBuilder(application.applicationContext,
+        AppDatabase::class.java, "newsDb").build()
+
+    @Provides
+    fun provideArticleDao(appDatabase: AppDatabase): ArticleDao {
+        return appDatabase.articleDao()
+    }
+
+    @BaseUrl
+    @Provides
+    fun provideBaseUrl(): String = "https://newsapi.org/v2/"
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideNetworkService(
+        @BaseUrl baseUrl: String,
+        gsonConverterFactory: GsonConverterFactory,
+        client: OkHttpClient
+    ): NetworkService {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+            .create(NetworkService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun createHttpClient() : OkHttpClient {
+        val builder = OkHttpClient().newBuilder()
+            .addInterceptor(KeyInterceptor())
+        return builder.build()
+    }
 
 }
