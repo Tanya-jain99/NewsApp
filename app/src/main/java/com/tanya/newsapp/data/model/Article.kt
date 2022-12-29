da170c4470c0f61b20d1520c244346b8cbac2cd7
Tanya jain <tjain@lolofit.com>
2022-12-30 02:23:17 +0530

    chore: added data layers

--- app/src/main/java/com/tanya/newsapp/data/model/Article.kt
+++ app/src/main/java/com/tanya/newsapp/data/model/Article.kt
@@ -1,16 +1,10 @@
 package com.tanya.newsapp.data.model
 
-import com.google.gson.annotations.SerializedName
 
 data class Article(
-    @SerializedName("title")
-    val title: String = "",
-    @SerializedName("description")
-    val description: String = "",
-    @SerializedName("url")
-    val url: String = "",
-    @SerializedName("urlToImage")
-    val imageUrl: String = "",
-    @SerializedName("source")
-    val source: Source
+    val title : String,
+    val description : String? = "",
+    val url : String,
+    val urlToImage : String?,
+    val source : Source
 )
